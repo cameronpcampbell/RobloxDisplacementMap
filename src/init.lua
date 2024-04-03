6781bc0d1d19675fbd8d7f6385ba0db6e674f06e
Cameron Campbell <cameron.campbell325@gmail.com>
2024-04-03 02:00:42 +0100

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -2,51 +2,65 @@
 
 local AssetService = game:GetService("AssetService")
 
-return function(
-    res: number, scale: number, amplitude: number, surfaceAppearance: SurfaceAppearance
-)
-    local resBounds = res + 1
-    
-    local eMesh = Instance.new("EditableMesh")
-    local eImage = AssetService:CreateEditableImageAsync(surfaceAppearance:GetAttribute("HeightMap"))
-    local eImageNewSize = Vector2.new(resBounds, resBounds)
-    eImage:Resize(eImageNewSize)
-    local eImagePixels = eImage:ReadPixels(Vector2.zero, eImage.Size)
-    
-    local verts = {}
-    
-    local i = 0
-    for x = 1, resBounds do
-        for z = 1, resBounds do
-            i += 1
-            
-            local start = (z - 1) * resBounds * 4 + (x - 1) * 4 + 1
-            local r, g, b = eImagePixels[start], eImagePixels[start + 1], eImagePixels[start + 2]
-            local height = ((r + g + b) / 3) * amplitude
-            
-            local vertId = eMesh:AddVertex(Vector3.new(x, height, z))
-            verts[i] = vertId
-            eMesh:SetUV(vertId, Vector2.new(x / resBounds, z / resBounds))
-        end
-    end
-    
-    for x = 1, res do
-        for z = 1, res do
-            local vertA, vertB, vertC, vertD = nil, nil, nil, nil
-            local colStart, colEnd = (z-1) * (res+1), z * (res+1)
-            local vertA, vertB, vertC, vertD =  verts[colStart + x], verts[colStart + x + 1], verts[colEnd + x], verts[colEnd + x + 1]
+local function GetRgbaFromPixelsArray(pixelsArray: { number }, startIdx: number)
+	return pixelsArray[startIdx], pixelsArray[startIdx + 1], pixelsArray[startIdx + 2], pixelsArray[startIdx + 3]
+end
 
-            eMesh:AddTriangle(vertA, vertB, vertC)
-            eMesh:AddTriangle(vertD, vertC, vertB)
-        end
-    end
-    
-    local mesh = Instance.new("MeshPart")
-    mesh.Size = Vector3.one * scale
-    mesh.Anchored = true
-    eMesh.Parent = mesh
-    surfaceAppearance:Clone().Parent = mesh
-    mesh.Parent = workspace
-    
-    return mesh
+return function(baseMesh: MeshPart, amplitude: number)
+	local surfaceAppearance = baseMesh:FindFirstChildOfClass("SurfaceAppearance")
+	assert(surfaceAppearance, "RobloxDisplacementMap: The MeshPart Doesn't Have A SurfaceAppearance!")
+	
+	local heightMapId = surfaceAppearance:GetAttribute("HeightMap")
+	assert(heightMapId and type(heightMapId) == "string", "RobloxDisplacementMap: The SurfaceAppearance needs a string attribute called \"HeightMap\"!")
+	
+	local heightMap = AssetService:CreateEditableImageAsync(heightMapId)
+	local heightMapSize = heightMap.Size
+	local heightMapSizeX, heightMapSizeY = heightMapSize.X, heightMapSize.Y
+	local heightMapPixelsArray = heightMap:ReadPixels(Vector2.zero, heightMapSize)
+
+	local mesh = baseMesh:Clone()
+	mesh.Parent = baseMesh.Parent
+	mesh.Name = "DisplacedMesh"
+	mesh.Transparency = 0
+	mesh.Parent = workspace
+
+	baseMesh.Transparency = 1
+	baseMesh.Locked = true
+
+	local eMesh = AssetService:CreateEditableMeshFromPartAsync(baseMesh)
+	eMesh.Parent = mesh
+	
+	local verts = eMesh:GetVertices() :: { number }
+	
+	local savedNewPositions: { [Vector3]: Vector3 } = {}
+	for iter, vertId in verts do
+		local vertPos = eMesh:GetPosition(vertId) :: Vector3
+		
+		local vertNewPos = savedNewPositions[vertPos]
+		if vertNewPos then
+			eMesh:SetPosition(vertId, vertNewPos)
+			continue
+		end
+		
+		local vertNormal, vertUv = eMesh:GetVertexNormal(vertId), eMesh:GetUV(vertId) :: Vector2
+		
+		local vertUvToImageX, vertUvToImageY =
+			math.floor(math.abs(vertUv.X % 1) * heightMapSizeX), math.floor(math.abs(vertUv.Y % 1) * heightMapSizeY)
+		
+		local startIdx = (vertUvToImageY - 1) * heightMapSizeX * 4 + (vertUvToImageX - 1) * 4 + 1
+		local r, g, b = GetRgbaFromPixelsArray(heightMapPixelsArray, startIdx)
+		if r == nil or g == nil or b == nil then continue end
+		local vertHeightColor = ((r + g + b) / 3)
+
+		local cf = CFrame.new(vertPos, vertNormal.Unit)
+		cf += (-cf.LookVector) * (vertHeightColor * amplitude)
+		local vertNewPos = cf.Position
+
+		eMesh:SetPosition(vertId, vertNewPos)
+		
+		savedNewPositions[vertPos] = vertNewPos
+	end
+	savedNewPositions = nil :: any
+	
+	return mesh
 end
